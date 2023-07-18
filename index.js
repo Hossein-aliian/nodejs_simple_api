b48b3157a3dc0a07b7dd6f0b4968a4281bb74f16
Hossein-aliian <101664111+Hossein-aliian@users.noreply.github.com>
2023-07-19 01:55:11 +0330

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+// const PORT =  3000;
+
+app.use(bodyParser.json());
+app.use(cors())
+let data = [];
+
+// ...................POST..........................
+app.post('/data/save', (req, res) => {
+  const newData = req.body;
+  data.push(newData);
+  res.send('اطلاعات با موفقیت اضافه شد');
+});
+// ...................POST..........................
+
+// ...................PUT..........................
+app.put('/data/save', (req, res) => {
+  const updatedData = req.body;
+  data.push(...updatedData);
+  res.send('اطلاعات با موفقیت به روز شد');
+});
+// ...................PUT..........................
+
+// ...................GET..........................
+app.get('/data', (req, res) => {
+  res.json(data);
+});
+// ...................GET..........................
+
+app.listen(PORT, () => {
+  console.log(`SERVER IS RUN:   http://localhost:${PORT}/data`);
+});
